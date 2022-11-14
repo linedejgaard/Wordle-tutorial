ca6ad9c108aa597b136f588f3ebf5f41c8106af4
ldej <ldej@itu.dk>
2022-11-14 20:34:20 +0100

    if-else-statement

--- Keyboard.java
+++ Keyboard.java
@@ -4,7 +4,6 @@ public class Keyboard {
     private char[] row3 = {'Z', 'X', 'C', 'V', 'B', 'N', 'M'};
     private char[][] letters = {row1, row2, row3};
 
-
     public Keyboard() {
     }
 
@@ -13,7 +12,61 @@ public class Keyboard {
     }
 
     public int getNumberInAlphabet(char letter) {
-        return ((int) letter) - 65;
+        if (letter == 'A') {
+            return 0;
+        } else if (letter == 'B') {
+            return 1;
+        } else if (letter == 'C') {
+            return 2;
+        } else if (letter == 'D') {
+            return 3;
+        } else if (letter == 'E') {
+            return 4;
+        } else if (letter == 'F') {
+            return 5;
+        } else if (letter == 'G') {
+            return 6;
+        } else if (letter == 'H') {
+            return 7;
+        } else if (letter == 'I') {
+            return 8;
+        } else if (letter == 'J') {
+            return 9;
+        } else if (letter == 'K') {
+            return 10;
+        } else if (letter == 'L') {
+            return 11;
+        } else if (letter == 'M') {
+            return 12;
+        } else if (letter == 'N') {
+            return 13;
+        } else if (letter == 'O') {
+            return 14;
+        } else if (letter == 'P') {
+            return 15;
+        } else if (letter == 'Q') {
+            return 16;
+        } else if (letter == 'R') {
+            return 17;
+        } else if (letter == 'S') {
+            return 18;
+        } else if (letter == 'T') {
+            return 19;
+        } else if (letter == 'U') {
+            return 20;
+        } else if (letter == 'V') {
+            return 21;
+        } else if (letter == 'W') {
+            return 22;
+        }else if (letter == 'X') {
+            return 23;
+        } else if (letter == 'Y') {
+            return 24;
+        } else if (letter == 'Z') {
+            return 25;
+        }
+        return -1;
+        //return ((int) letter) - 65;
     }
 
 }
